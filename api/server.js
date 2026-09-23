a1b3ffd7a55aac45adfa2d9b346ded09d26e6add
okbaAbdelghani <okbaabdelghani88@gmail.com>
2026-09-23 14:27:48 +0000

    Add HTTP requests metrics

--- api/server.js
+++ api/server.js
@@ -4,11 +4,51 @@ const { Pool } = require("pg");
 const client = require("prom-client");
 
 const app = express();
-app.use(express.json());
 
 
 const register = new client.Registry();
 client.collectDefaultMetrics({ register });
+
+const httpRequestsTotal = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "status_code"],
+  registers: [register],
+});
+
+const httpRequestDuration = new client.Histogram({
+  name: "http_request_duration_seconds",
+  help: "Duration of HTTP requests in seconds",
+  labelNames: ["method", "route", "status_code"],
+  registers: [register],
+  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5],
+});
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const start = process.hrtime.bigint();
+
+  res.on("finish", () => {
+    const duration = Number(process.hrtime.bigint() - start) / 1_000_000_000;
+
+    const route = req.route?.path || req.path;
+    httpRequestsTotal.inc({
+      method: req.method,
+      route: route,
+      status_code: res.statusCode,
+    });
+
+    httpRequestDuration.observe({
+      method: req.method,
+      route: route,
+      status_code: res.statusCode,
+    }, duration);
+  });
+
+  next();
+})
+
 
 const PORT = process.env.PORT || 3000;
 
